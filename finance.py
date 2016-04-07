685149625726f2958c98b52f31f7318edc9080e3
Edwards <jbe@6c4008a5b474.ant.amazon.com>
2016-04-06 17:59:58 -0700

    updated finance.py to parse a csv file with your stocks.

--- finance.py
+++ finance.py
@@ -1,12 +1,29 @@
 from googlefinance import getQuotes
 import json
-from decimal import Decimal
+from decimal import Decimal,getcontext
+import os
+import csv
 
-stackInfo = json.dumps(getQuotes('AMZN'), indent=2)
-stockInfo = json.loads(stackInfo)
-closePrice = stockInfo[0]['LastTradePrice']
-close = Decimal(closePrice)
-numShares = 125
-shares = Decimal(numShares)
-stockVal = close * shares
-print stockVal
+confFile = os.path.expanduser('~/github/portfoliotracker/stocks.txt')
+
+#config.read(confFile)
+
+def getQuote(stockTicker):
+    stackInfo = json.dumps(getQuotes(stockTicker), indent=2)
+    stockInfo = json.loads(stackInfo)
+    closePrice = stockInfo[0]['LastTradePrice']
+    getcontext().prec = 3
+    close = Decimal(closePrice)
+    return close
+
+value = 0
+
+with open(confFile, 'rb') as csvfile:
+    stockReader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for ticker,quantity in stockReader:
+        stockVal = getQuote(ticker)
+        getcontext().prec = 10
+        value += stockVal * Decimal(quantity)
+        print "ticker: "+ticker+" quantity: "+quantity + " stockValue: ",stockVal
+
+print "value of stock portfolio is:", value
